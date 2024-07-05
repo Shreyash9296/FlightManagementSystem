2b9ec6fce616a02651c42b1c436abe4a23db1dfd
Prathmesh_Jaiswal <90593687+prathmeshjaiswal@users.noreply.github.com>
2024-07-05 15:38:34 +0530

    adding user login

--- fms-backend/src/main/java/com/spring/fms_backend/FmsBackendApplication.java
+++ fms-backend/src/main/java/com/spring/fms_backend/FmsBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FmsBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FmsBackendApplication.class, args);
 	}
 
