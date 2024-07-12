9862f23e9142b61fa287c97a01f0cb246fe342ae
Prathmesh_Jaiswal <90593687+Prathmeshjaiswal@users.noreply.github.com>
2024-07-12 10:44:06 +0530

    Delete fms-backend/src/main/java/com/spring directory

--- fms-backend/src/main/java/com/spring/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.string.repository;
-
-import  com.spring.model.Payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<payment,long>{
-
-}
--- fms-backend/src/main/java/com/spring/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.repository;
-
-import com.spring.model.user;
-import org.springFramework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<user , Long>{
-    user findByUsername(String username);
-}
